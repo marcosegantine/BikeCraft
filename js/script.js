0471df33d7e4f8fdf42c4c21e389ec9a5a0e1783
Marcos Segantine <marcosegantine@hotmail.com>
2022-10-20 21:01:14 +0100

    commit do projeto BikeCraft desenvido no curso basico de de desenvolvimento frontend(ORIGAMID)

---
+++ js/script.js
@@ -0,0 +1,18 @@
+if (window.SimpleAnime) {
+new SimpleAnime();
+}
+
+if (window.SimpleAnime) {
+  new SimpleSlide({
+    slide:"quote",
+    auto: true,
+    time: 4000
+  });
+
+  new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+  }
+
